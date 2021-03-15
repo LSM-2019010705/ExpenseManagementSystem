1300af576438a473e6734eeb22c69d28d09e678a
LSM-2019010705 <dltmd127@gmail.com>
2021-03-16 01:09:22 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add expense");
+		System.out.println("2. Delet expense");
+		System.out.println("3. Edit expense");
+		System.out.println("4. View expense");
+		System.out.println("5. Exit");
+	}
+
+}
