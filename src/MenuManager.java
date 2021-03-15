c6fb21a2ccc95021b3422961df0785fbacf63437
LSM-2019010705 <dltmd127@gmail.com>
2021-03-16 07:31:05 +0900

    changed menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,14 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add expense");
-		System.out.println("2. Delet expense");
-		System.out.println("3. Edit expense");
-		System.out.println("4. View expense");
+		System.out.println("1. Extra expense");// 추가되는 비용들
+		
+		System.out.println("2. Decrease expense");//감소된 비용들
+		
+		System.out.println("3. Edit expense");//수정
+		
+		System.out.println("4. Detailed statment");//내역서
+		
 		System.out.println("5. Exit");
 	}
 
