3fb1b6fddde52f3a9c465b2339629a335d7c22d4
LSM-2019010705 <dltmd127@gmail.com>
2021-05-18 02:46:27 +0900

    refactoring & error handling

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManager {
@@ -6,35 +7,54 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		AccountManager accountmanager = new AccountManager(input);
 		
+		selectMenu(input, accountmanager);
+	}
+	
+	public static void selectMenu(Scanner input, AccountManager accountmanager) {
 		int num=-1;
 		while (num != 6) {
-			System.out.println("----- 메뉴 번호를 선택하십시오. -----");
-			System.out.println("1. 계좌 추가"); //메뉴 목록들이다.
-			System.out.println("2. 계좌 삭제");
-			System.out.println("3. 계좌 편집");
-			System.out.println("4. 계좌 목록");
-			System.out.println("5. 계좌 보기");
-			System.out.println("6. Exit");
-			System.out.println("Select one number between 1~6");
+			try {
+			showMenu();
 			num = input.nextInt(); //메뉴를 입력받는다
-			
-			
-			if (num == 1) {
-				accountmanager.addAccount();
+			switch(num) {
+				case 1:
+					accountmanager.addAccount();
+					break;
+				case 2:
+					accountmanager.deleteAccount(); 
+					break;
+				case 3:
+					accountmanager.editAccount();
+					break;
+				case 4:
+					accountmanager.listAccount();
+					break;
+				case 5:
+					accountmanager.viewAccounts();
+					break;
+				default:
+					continue;
+				}
 			}
-			else if (num == 2) {
-				accountmanager.deleteAccount(); 
-			}
-			else if (num == 3) {
-				accountmanager.editAccount();
-			}
-			else if (num == 4) {
-				accountmanager.listAccount();
-			}
-			else if (num == 5) {
-				accountmanager.viewAccounts();
+			catch(InputMismatchException e){
+				System.out.println("Please put in an integer!");
+				if (input.hasNext()) {
+					input.next();
+				}
+				num = -1;
 			}
 		}
+	}
+	
+	public static void showMenu() {
+		System.out.println("----- 메뉴 번호를 선택하십시오. -----");
+		System.out.println("1. 계좌 추가"); //메뉴 목록들이다.
+		System.out.println("2. 계좌 삭제");
+		System.out.println("3. 계좌 편집");
+		System.out.println("4. 계좌 목록");
+		System.out.println("5. 계좌 보기");
+		System.out.println("6. Exit");
+		System.out.println("Select one number between 1~6: ");
 	}
 			
 }
